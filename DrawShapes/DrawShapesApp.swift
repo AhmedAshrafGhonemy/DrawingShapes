d69a5423e7161dba71294e4889906b8e03dfb4bf
AhmedAshrafghonemy <AhmedAshrafghonemy@gmail.com>
2021-12-22 23:30:40 +0200

    Initial Commit

---
+++ DrawShapes/DrawShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawShapesApp.swift
+//  DrawShapes
+//
+//  Created by macbook on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
